931b5da46c991852f2fc6635e9080e9da7338034
Douglass Hooker <DouglassHooker@gmail.com>
2019-04-11 15:28:55 -0400

    Update utils.js
    Add labeling

--- utils.js
+++ utils.js
@@ -1,21 +1,21 @@
 
 String.prototype.matchAll = String.prototype.matchAll || function (pattern) {
-    const matches = []
+    var matches = []
     this.replace(pattern, function() {
       // arguments[0] is the entire match
       // args 1 - 2 before end of array are the capture groups
-      const subMatches = [...arguments]
+      var subMatches = [...arguments]
       matches.push(subMatches)
     })
     return matches
 }
-const stringToTest = '<<INVALID FIELD [[Test]]>> test <<INVALID FIELD [[test]]>> <<INVALID FIELD [[test]]>>'
-console.log(stringToTest.matchAll(/<<.*?\[\[(.*?)\]\]>>/g))
+var stringToTest = '<<INVALID FIELD [[Test]]>> test <<INVALID FIELD [[test]]>> <<INVALID FIELD [[test]]>>'
+console.log('matchall test', stringToTest.matchAll(/<<.*?\[\[(.*?)\]\]>>/g))
 
 
 // Template system
 
-const templateContent = `
+var templateContent = `
     <div>
         <p>{{ title }}</p>
         <div>{{ content }}</div>
@@ -28,4 +28,4 @@ const pattern = /\{\{\s?(.*)\s?\}\}/g;
 const templateMatches = templateContent.matchAll(pattern)
 
 // Expect something like [[ '{{ title }}', 'templateVar', 'position', 'originalString' ] ...]
-console.log(templateMatches)
+console.log('templates', templateMatches)

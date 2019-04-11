66d854e4c9efecf0df860239ef47773239899ac8
Douglass Hooker <DouglassHooker@gmail.com>
2019-04-11 15:23:49 -0400

    Update utils.js
    Add template example

--- utils.js
+++ utils.js
@@ -1,12 +1,31 @@
+
 String.prototype.matchAll = String.prototype.matchAll || function (pattern) {
-  var matches = []
-  this.replace(pattern, function() {
-    // arguments[0] is the entire match
-    // args 1 - 2 before end of array are the capture groups
-    var subMatches = [...arguments]
-    matches.push(subMatches)
-  })
-  return matches
+    const matches = []
+    this.replace(pattern, function() {
+      // arguments[0] is the entire match
+      // args 1 - 2 before end of array are the capture groups
+      const subMatches = [...arguments]
+      matches.push(subMatches)
+    })
+    return matches
 }
-var stringToTest = '<<INVALID FIELD [[Test]]>> test <<INVALID FIELD [[test]]>> <<INVALID FIELD [[test]]>>'
+const stringToTest = '<<INVALID FIELD [[Test]]>> test <<INVALID FIELD [[test]]>> <<INVALID FIELD [[test]]>>'
 console.log(stringToTest.matchAll(/<<.*?\[\[(.*?)\]\]>>/g))
+
+
+// Template system
+
+const templateContent = `
+    <div>
+        <p>{{ title }}</p>
+        <div>{{ content }}</div>
+    </div>
+`
+
+// template patterns
+const pattern = /\{\{\s?(.*)\s?\}\}/g;
+
+const templateMatches = templateContent.matchAll(pattern)
+
+// Expect something like [[ '{{ title }}', 'templateVar', 'position', 'originalString' ] ...]
+console.log(templateMatches)
